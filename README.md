2ab24ad3aaf74754146c8c0679d708506bf50be7
Vladimir Kovpak <cn007b@gmail.com>
2017-03-22 10:04:26 +0200

    v1

---
+++ README.md
@@ -0,0 +1,1 @@
+`curl -XPOST localhost:8080/zf3eqk3mxtmtc8pwwuv50zfr -H 'Content-Type: application/json' -d '{"c":"200"}'`
---
+++ src/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var socketIo = require('socket.io');
+
+var server = app.listen(80, function () {});
+var socket = socketIo.listen(server);
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function (req, res) {
+    var streamId = Math.random().toString(36).substring(2);
+    res.redirect('/' + streamId);
+});
+
+app.get('/:streamId?', function (req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/:streamId?', function (req, res) {
+    if (req.headers['content-type'] === 'application/json') {
+        socket.emit('log', {streamId: req.params.streamId, format: 'json', data: req.body});
+    }
+    res.send('');
+});
