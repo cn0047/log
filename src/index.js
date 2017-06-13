c364969bae9f049ac37715c4748d806690d8bd11
Vladimir Kovpak <cn007b@gmail.com>
2017-06-13 19:47:17 +0300

    Create index.js

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
