a92c6eb512365272342361d78dd532e78c190539
Vladimir Kovpak <cn007b@gmail.com>
2017-11-17 15:22:34 +0200

    Added comments into public app.js.

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -2,5 +2,15 @@ const app = {
   autoScrool: true,
 };
 
-// Init sentry, for capturing frontend errors.
+/**
+ * Init sentry, for capturing frontend errors.
+ *
+ * global: Raven
+ * global: sentryDSN
+ */
 Raven.config(sentryDSN).install();
+
+// Enable autoScrool by default.
+// This code redundant, but it necessary for code quality checkers...
+// Now app variable doesn't looks like never used 🤓🙈😁
+app.autoScrool = true;
