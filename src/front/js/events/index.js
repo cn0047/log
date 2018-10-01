71bcca5fd06bb041542b9a44329ab78cd8a0228c
Vladimir Kovpak <cn007b@gmail.com>
2018-10-02 00:17:05 +0300

    Updated js for events.

--- src/front/js/events/index.js
+++ src/front/js/events/index.js
@@ -2,21 +2,6 @@ const app = require('./../app');
 const { trackClickCodeExample } = require('./../services/googleAnalytics');
 
 /**
- * Handler for menu "news" link, which is open menu block.
- */
-document.getElementById('menuNews').addEventListener('click', () => {
-  const newsStyle = document.getElementById('news').style;
-  newsStyle.display = (newsStyle.display === 'none') ? 'block' : 'none';
-});
-
-/**
- * Handler for close image, which is close (hide) parent element.
- */
-document.getElementById('close').addEventListener('click', (e) => {
-  e.target.parentElement.style.display = 'none';
-});
-
-/**
  * Event for click on block "codeExample" - just track into GoogleAnalytics.
  */
 document.querySelectorAll('pre.codeExample').forEach((el) => {
